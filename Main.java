263bf43cb033578a156c6f89750f2702af09c831
Mateusz0231 <100692029+Mateusz0231@users.noreply.github.com>
2022-03-15 12:20:48 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+    for(int i=2; i<=100; i++)
+      {
+        System.out.println(i++);
+      }
+    System.out.println("Koniec");
+  }
+}
